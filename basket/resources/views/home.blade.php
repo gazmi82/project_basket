109c54b8e6ce2a66c7eee5a258fb65f89fb3e779
gazmir <gazmir.sulcaj@epitech.eu>
2020-06-25 14:52:47 +0200

    Home Front

--- basket/resources/views/home.blade.php
+++ basket/resources/views/home.blade.php
@@ -1,23 +1,100 @@
 @extends('layouts.app')
 
+@section('title')
+Home
+@endsection
+
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <div class="card bg-dark text-white">
+  <img src="https://www.cars.com/cldstatic/ads/creative/20200316_4876_Nissan_Hero_2400x753.jpg" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title">2020 Nissan Rogue</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-danger">Buy</a>
+      <button type="button" class="btn btn-success">Add to Cart</button>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
+<br></br>
+    <div class="card-deck">
+  <div class="card">
+    <img src="https://www.cstatic-images.com/car-pictures/main/USD00AUS052A021001.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Audi</h5>
+      <p class="card-text">2021 Audi Q3</p>
+      <a href="#" class="btn btn-danger">Buy</a>
+      <button type="button" class="btn btn-success">Add to Cart</button>
     </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://www.cstatic-images.com/car-pictures/main/USD10AUC311A021001.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Audi</h5>
+      <p class="card-text">20201 Audi RS 7</p>
+      <a href="#" class="btn btn-danger">Buy</a>
+      <button type="button" class="btn btn-success">Add to Cart</button>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://www.cstatic-images.com/car-pictures/main/usd00mbc682a021001.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Mercedez-Benz</h5>
+      <p class="card-text">2021 Mercedez-Benz E-Class</p>
+      <a href="#" class="btn btn-danger">Buy</a>
+      <button type="button" class="btn btn-success">Add to Cart</button>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+<br></br>
+<div class="card-deck">
+  <div class="card">
+    <img src="https://www.cstatic-images.com/car-pictures/main/USD00AUC016A021001.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Audi</h5>
+      <p class="card-text">2021 Audi A4</p>
+      <a href="#" class="btn btn-danger">Buy</a>
+      <button type="button" class="btn btn-success">Add to Cart</button>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://www.cstatic-images.com/car-pictures/main/USC80ALS011A021001.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Alfa Romeo Stelvio</h5>
+      <p class="card-text">2020 Alfa Romeo Stelvio</p>
+      <a href="#" class="btn btn-danger">Buy</a>
+      <button type="button" class="btn btn-success">Add to Cart</button>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://www.cstatic-images.com/car-pictures/main/usc90mnc092b021001.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Mini</h5>
+      <p class="card-text">2020 Mini Hardtop</p>
+      <a href="#" class="btn btn-danger">Buy</a>
+      <button type="button" class="btn btn-success">Add to Cart</button>
+      <button type="button" class="btn btn-link">Price</button>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+    
 </div>
 @endsection
--- basket/resources/views/layouts/app.blade.php
+++ basket/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Basket') }}</title>
+    <title>@yield('title')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -15,6 +15,8 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -38,43 +40,64 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                        <li class="nav-item">
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                            <form method="GET" action="{{ url('my-search') }}">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <input type="text" name="search" class="form-control" placeholder="Search" value="{{ old('search') }}">
+                              </div>
+                              <div class="col-md-6">
+                                  <button class="btn btn-success">Search</button>
+                              </div>
+                          </div>
+                      </form>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Shopping cart <span class="sr-only">(current)</span></a>
+                </li>
+                
+                <!-- Authentication Links -->
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+            </li>
+            @endguest
+        </ul>
     </div>
+</div>
+</nav>
+</div>
+
+
+
+<main class="py-4">
+
+    @yield('content')
+</main>
+</div>
 </body>
 </html>
